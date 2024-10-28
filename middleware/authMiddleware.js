5c8c646bb569d37cc3f5f29511f48d1d91a7358d
Elvitigala E.D.T.V <99307615+IT21176838@users.noreply.github.com>
2024-10-28 09:05:49 +0530

    200 memebrs

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -41,37 +41,3 @@ export { protect };
 
 
 
-// import jwt from "jsonwebtoken";
-// import User from "../models/memberModel.js";
-
-// // Create a middleware for JWT authentication
-
-// const protect = async (req, res, next) => {
-//     try {
-//       let token;
-  
-//       token = req.cookies.jwt;
-  
-//       if (token) {
-//         try {
-//           // Create a decoded user object from the token
-//           const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//           // Assign the decoded user to current user
-//           req.user = await User.findById(decoded.userId).select("-password");
-  
-//           next();
-//         } catch (error) {
-//           res.status(401);
-//           throw new Error("Invalid Token");
-//         }
-//       } else {
-//         res.status(401);
-//         throw new Error("Not authorized, No Token");
-//       }
-//     } catch (error) {
-//       res.status(401).json({ message: error.message });
-//     }
-//   };
-  
-//   export { protect };
-  
