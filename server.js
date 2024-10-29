11451dd683f9385456c0f6bab6b3f0c766e0ee2a
Hasitha <hasithapeiris.me@gmail.com>
2024-10-29 22:22:43 +0530

    fix: fix cors issue

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
-import loanRoutes from './routes/loanRoutes.js';
+import loanRoutes from "./routes/loanRoutes.js";
 import cors from "cors";
 import ServerlessHttp from "serverless-http";
 
@@ -14,9 +14,9 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-app.get('/.netlify/functions/api', (req, res) => {
+app.get("/.netlify/functions/api", (req, res) => {
   return res.json({
-    message: 'Hello from serverless function'
+    message: "Hello from serverless function",
   });
 });
 
@@ -28,16 +28,15 @@ app.use(express.urlencoded({ extended: true }));
 // Extract cookie data from HTTP requests
 app.use(cookieParser());
 
-//app.use(cors());
+app.use(cors());
 
-app.use(cors({
-  origin: 'http://localhost:5173', // Update to your frontend URL
-  credentials: true, // Allow cookies to be sent across origins
-}));
-
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true,
+// }));
 
 app.use("/api/members", userRoutes);
-app.use('/api/loans', loanRoutes);
+app.use("/api/loans", loanRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
@@ -48,8 +47,6 @@ app.listen(PORT, () => {
 
 // Export the handler function for use in Netlify Functions
 export const handler = async (event, context) => {
-    const result = await ServerlessHttp(app)(event, context);
-    return result;
+  const result = await ServerlessHttp(app)(event, context);
+  return result;
 };
-
-
