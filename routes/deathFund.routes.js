25832dd25224693c81aca2a52c0a5ad891eb8767
Hasitha <hasithapeiris.me@gmail.com>
2024-12-17 12:09:41 +0530

    feat: create routes for death fund APIs

--- routes/deathFund.routes.js
+++ routes/deathFund.routes.js
@@ -1,9 +1,25 @@
 import express from "express";
-import { createDeathFund } from "../controllers/deathFund.controller.js";
+import {
+  createDeathFund,
+  viewAllDeathFunds,
+  viewSingleDeathFund,
+  updateDeathFund,
+  deleteDeathFund,
+} from "../controllers/deathFund.controller.js";
 import { isAdmin, protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, isAdmin, createDeathFund);
+router
+  .route("/")
+  .post(protect, isAdmin, createDeathFund)
+  .get(protect, isAdmin, viewAllDeathFunds);
+
+router
+  .route("/:id")
+  .get(protect, viewSingleDeathFund)
+  .put(protect, isAdmin, updateDeathFund)
+  .delete(protect, isAdmin, deleteDeathFund);
 
 export default router;
